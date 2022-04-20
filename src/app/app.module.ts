40285748f72d048e09167c41e594a2342b1126c5
Ruslan-c0de <60577238+Ruslan-c0de@users.noreply.github.com>
2022-04-20 11:33:18 +0300

    start window strukture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StartWindowComponent } from './start-window/start-window.component';
+
+const appRoutes: Routes = [
+  {path: '', component: StartWindowComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartWindowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
